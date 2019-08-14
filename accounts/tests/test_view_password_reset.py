7e6d246e97e12f51f37b83c7c44e7c13127216f2
Antonio Salaices Monzón <antonio.salaicesm@gmail.com>
2019-08-14 16:01:49 -0600

    cambios en los test y agrego templates

--- accounts/tests/test_view_password_reset.py
+++ accounts/tests/test_view_password_reset.py
@@ -18,7 +18,7 @@ class PasswordResetConfirmTests(TestCase):
         based on how django creates the token internally:
         https://github.com/django/django/blob/1.11.5/django/contrib/auth/forms.py#L280
         '''
-        self.uid = urlsafe_base64_encode(force_bytes(user.pk)).decode()
+        self.uid = urlsafe_base64_encode(force_bytes(user.pk)).decode('utf-8')
         self.token = default_token_generator.make_token(user)
 
         url = reverse('password_reset_confirm', kwargs={'uidb64': self.uid, 'token': self.token})
@@ -49,7 +49,7 @@ class PasswordResetConfirmTests(TestCase):
 class InvalidPasswordResetConfirmTests(TestCase):
     def setUp(self):
         user = User.objects.create_user(username='john', email='john@doe.com', password='123abcdef')
-        uid = urlsafe_base64_encode(force_bytes(user.pk)).decode()
+        uid = urlsafe_base64_encode(force_bytes(user.pk)).decode('utf-8')
         token = default_token_generator.make_token(user)
 
         '''
--- boards/views.py
+++ boards/views.py
@@ -3,6 +3,7 @@ from django.contrib.auth.models import User
 from django.shortcuts import render, redirect, get_object_or_404
 from .models import Board, Topic, Post
 from .forms import NewTopicForm
+from django.contrib.auth.decorators import login_required
 
 def home(request):
     boards = Board.objects.all()
@@ -12,26 +13,30 @@ def home(request):
 def board_topics(request, pk):
     board = get_object_or_404(Board, pk=pk)
     return render(request, 'topics.html', {'board': board})
-
+    
+def topic_posts(request, pk, topic_pk):
+    topic = get_object_or_404(Topic, board__pk=pk, pk=topic_pk)
+    return render(request, 'topic_posts.html', {'topic': topic})
+    
+@login_required
 def new_topic(request, pk):
     board = get_object_or_404(Board, pk=pk)
-    user = User.objects.first()  
     if request.method == 'POST':
         form = NewTopicForm(request.POST)
         if form.is_valid():
             topic = form.save(commit=False)
             topic.board = board
-            topic.starter = user
+            topic.starter = request.user  # <- here
             topic.save()
-            post = Post.objects.create(
+            Post.objects.create(
                 message=form.cleaned_data.get('message'),
                 topic=topic,
-                created_by=user
+                created_by=request.user  # <- and here
             )
             return redirect('board_topics', pk=board.pk)  
     else:
         form = NewTopicForm()
-    return render(request, 'new_topic.html', {'board': board, 'form': form}) 
+    return render(request, 'new_topic.html', {'board': board, 'form': form})
 
 
 
