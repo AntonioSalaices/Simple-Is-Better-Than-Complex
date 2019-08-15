c97ff9637340274b50846b113239c1a1eb2003c6
Antonio Salaices Monzón <antonio.salaicesm@gmail.com>
2019-08-15 15:50:52 -0600

    todo funcional

--- boards/forms.py
+++ boards/forms.py
@@ -1,13 +1,16 @@
+    
 from django import forms
-from .models import Topic, Post
+
+from .models import Post, Topic
+
 
 class NewTopicForm(forms.ModelForm):
     message = forms.CharField(
         widget=forms.Textarea(
-            attrs={'rows': 5, 'placeholder': '¿Que estas pensando?'}
+            attrs={'rows': 5, 'placeholder': 'What is in your mind?'}
         ),
         max_length=4000,
-        help_text='La longitud maxima por mensaje es de 4000.'
+        help_text='The max length of the text is 4000.'
     )
 
     class Meta:
@@ -18,4 +21,4 @@ class NewTopicForm(forms.ModelForm):
 class PostForm(forms.ModelForm):
     class Meta:
         model = Post
-        fields = ['message', ]        
+        fields = ['message', ]    
